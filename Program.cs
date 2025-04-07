cfd12f3843e4cec9be4ded93025553094d6b1a6f
Kierialt <holyangel.27@mail.ru>
2025-04-07 12:27:27 +0200

    Commit 1

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Cars.Clsses;
+
+
+class Program
+{
+    static void Main()
+    {
+        Car myCar = new Car("Toyota", "Corolla", 2023, 10); // Создаем машину с 10 литрами топлива
+        myCar.DisplayInfo();
+
+        Console.WriteLine("\nЕдем 100 км:");
+        myCar.Drive(100);
+        myCar.DisplayInfo();
+
+        Console.WriteLine("\nПопробуем проехать 500 км (не хватит топлива):");
+        myCar.Drive(500);
+        myCar.DisplayInfo();
+
+        Console.WriteLine("\nЗаправляем 30 литров:");
+        myCar.Refuel(30);
+        myCar.DisplayInfo();
+
+        Console.WriteLine("\nЗаправляем 20 литров:");
+        myCar.Refuel(20);
+        myCar.DisplayInfo();
+
+        Console.WriteLine("\nПопробуем проехать 502 км (не хватит топлива):");
+        myCar.Drive(502);
+        myCar.DisplayInfo();
+        
+        Console.WriteLine("\nЗаправляем 200 литров (превышен бак):");
+        myCar.Refuel(200);
+        myCar.DisplayInfo();
+        
+        Console.WriteLine("\nПопробуем проехать 637 км (хватит топлива):");
+        myCar.Drive(637);
+        myCar.DisplayInfo();
+    }
+}
